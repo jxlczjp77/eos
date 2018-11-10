2569346c1c4e2ba9a8cba79cc69f053ecfb008eb
jxlczjp77 <zhang.jianping@bymiao.com>
2018-11-10 18:15:44 +0800

    添加部分文件

---
+++ programs/cleos/config.hpp
@@ -0,0 +1,18 @@
+/** @file 
+ *    @copyright defined in eos/LICENSE.txt 
+ *
+ * \warning This file is machine generated. DO NOT EDIT.  See config.hpp.in for changes.
+ */
+
+namespace eosio { namespace client { namespace config {
+   constexpr char version_str[] = "f9a3d023";
+   constexpr char locale_path[] = "/usr/local/eosio/share/locale";
+   constexpr char locale_domain[] = "cleos";
+#ifdef _MSC_VER
+   constexpr char key_store_executable_name[] = "keosd.exe";
+#else
+   constexpr char key_store_executable_name[] = "keosd";
+#endif
+}
+}
+}
